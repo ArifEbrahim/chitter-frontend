95826c33dd4d2ade7c8e26636080d075577518d1
Arif Ebrahim <74261924+ArifEbrahim@users.noreply.github.com>
2022-08-17 10:42:38 +0100

    Feature: AllPosts now displays API data

--- src/pages/AllPosts.js
+++ src/pages/AllPosts.js
@@ -1,7 +1,32 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 export default function AllPosts() {
-  return (
-    <div>AllPosts</div>
-  )
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const getPostData = async () => {
+      const URL = "https://chitter-backend-api-v2.herokuapp.com/peeps";
+      const response = await axios.get(URL);
+      setPosts(response.data);
+    };
+
+    getPostData();
+  }, []);
+
+  let content = <p>No posts found.</p>;
+
+  if (posts.length > 0) {
+    content = (
+      <>
+        <ul>
+          {posts.map((post) => (
+            <li key={post.id}>{post.body}</li>
+          ))}
+        </ul>
+      </>
+    );
+  }
+
+  return <section>{content}</section>;
 }
--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,7 +1,9 @@
-import React from 'react'
+import React from "react";
 
 export default function NotFound() {
   return (
-    <div>NotFound</div>
-  )
+    <div className="centered">
+      <p>Page not found</p>
+    </div>
+  );
 }
