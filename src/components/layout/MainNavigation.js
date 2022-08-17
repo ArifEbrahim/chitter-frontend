027aac3f36b877a7edf2db500a8cbe017cfb2410
Arif Ebrahim <74261924+ArifEbrahim@users.noreply.github.com>
2022-08-17 10:04:38 +0100

    Style: added styling to nav bar

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -1,17 +1,29 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 
+import classes from "./MainNavigation.module.css";
+
 export default function MainNavigation() {
   return (
-    <header>
-      <div>Chitter</div>
-      <nav>
+    <header className={classes.header}>
+      <div className={classes.logo}>Chitter</div>
+      <nav className={classes.nav}>
         <ul>
           <li>
-            <NavLink to="/posts">All posts</NavLink>
+            <NavLink
+              to="/posts"
+              className={({ isActive }) => (isActive ? classes.active : "")}
+            >
+              All posts
+            </NavLink>
           </li>
           <li>
-            <NavLink to="/new-post">Add a post</NavLink>
+            <NavLink
+              to="/new-post"
+              className={({ isActive }) => (isActive ? classes.active : "")}
+            >
+              Add a post
+            </NavLink>
           </li>
         </ul>
       </nav>
