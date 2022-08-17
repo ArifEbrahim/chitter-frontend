7b0074685d3aad6e6f63974fbf231c32db8f0548
Arif Ebrahim <74261924+ArifEbrahim@users.noreply.github.com>
2022-08-17 05:09:13 +0100

    Feature: added home redirect and not found page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 
 import Layout from "./components/layout/Layout";
 import AllPosts from "./pages/AllPosts";
 import NewPost from "./pages/NewPost";
+import NotFound from "./pages/NotFound"
 import PostDetail from "./pages/PostDetail";
 
 export default function App() {
   return (
     <Layout>
       <Routes>
+        <Route path="/" element={<Navigate to='/posts' replace />} />
         <Route path="/posts" element={<AllPosts />} />
         <Route path="/posts/:postId" element={<PostDetail />} />
         <Route path="/new-post" element={<NewPost />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </Layout>
   );
