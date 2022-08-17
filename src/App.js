0161d939b138ff74a0d9b48ab9f9c5ad0b719485
Arif Ebrahim <74261924+ArifEbrahim@users.noreply.github.com>
2022-08-17 04:47:51 +0100

    Feature: routing working and pages setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
-import React from 'react'
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+
+import AllPosts from "./pages/AllPosts";
+import NewPost from "./pages/NewPost";
+import PostDetail from "./pages/PostDetail";
 
 export default function App() {
   return (
-    <div>App</div>
-  )
+    <Routes>
+      <Route path="/posts" element={<AllPosts />} />
+      <Route path='/posts/:postId' element={<PostDetail />} />
+      <Route path='/new-post' element={<NewPost />} />
+    </Routes>
+  );
 }
